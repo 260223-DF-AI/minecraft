1e924c4ac1bcc3f0526b06c8f257800832be5095
Andy Mei <qyptn123@gmail.com>
2026-05-01 09:41:21 -0500

    analyst.py debugged and finished

--- agents/analyst.py
+++ agents/analyst.py
@@ -9,7 +9,8 @@ from pydantic import BaseModel
 
 from agents.state import ResearchState
 
-from langchain_ollama import OllamaChat
+from langchain_ollama import ChatOllama
+from langchain_core.documents import Document
 
 import json
 # ---------------------------------------------------------------------------
@@ -35,12 +36,11 @@ class AnalysisResult(BaseModel):
 # ---------------------------------------------------------------------------
 
 # code to build a prompt given some stuff from the state
-def build_prompt(question: str, sub_task: str, retrieved_chunks: list[dict]) -> str:
+def build_prompt(question: str, sub_task: str, retrieved_chunks: list[Document]) -> str:
     context = "\n\n".join(
-        f"[Source: {c['source']} | Page: {c.get('page_number')}]\n{c['text']}"
-        for c in retrieved_chunks
+        f"[Source: {doc.metadata['source']} | Page: {doc.metadata.get('page_number', 1)}]\n{doc.page_content}"
+        for doc in retrieved_chunks
     )
-
     return f"""
     You are a Minecraft expert.
 
@@ -62,12 +62,14 @@ def build_prompt(question: str, sub_task: str, retrieved_chunks: list[dict]) -> 
     - Extract atomic factual claims from your answer.
     - Each claim must be preferably verifiable from the context.
     - Include citations for every major statement if possible.
-
+    - Only use Citations if they are from the context.
+    - For confidence scores, a 10.0 would be directly verifiable from the context. Use lower score if this is not the case.
+    - For confidence scores, dock points 3-5 points if you used information from your own training instead of the context (do not dock if context has the answer).
     Return JSON with:
     - answer (string)
     - claims (list of strings)
     - citations (list of objects with source, page_number, excerpt)
-    - confidence (float between 0 and 1)
+    - confidence (float between 0.0 and 10.0)
 
     Return ONLY valid JSON.
 
@@ -86,18 +88,20 @@ def build_prompt(question: str, sub_task: str, retrieved_chunks: list[dict]) -> 
     }}
     """
 
+from langchain_ollama import ChatOllama
+
 def stream_ollama(prompt: str, model: str = "llama3.1:8b") -> str:
-    stream = OllamaChat(
-        model=model,
-        messages=[{"role": "user", "content": prompt}],
-        stream=True,
-    )
+    #print("Streaming response from Ollama... with prompt:", prompt)
+
+    llm = ChatOllama(model=model)
+
+    stream = llm.stream(prompt)
 
     full_response = ""
 
     for chunk in stream:
-        token = chunk["message"]["content"]
-        print(token, end="", flush=True)  # real-time output
+        token = chunk.content or ""
+        print(token, end="", flush=True)
         full_response += token
 
     print()  # newline after stream ends
@@ -130,6 +134,7 @@ def analyst_node(state: ResearchState) -> dict:
     
     analysis_json = ""
     try:
+        #print("got raw output:", raw_output)
         analysis_json = json.loads(raw_output) # attempt to parse json
         analysis = AnalysisResult(**analysis_json)
     except Exception as e:
@@ -143,3 +148,93 @@ def analyst_node(state: ResearchState) -> dict:
         "analysis": analysis.model_dump(),
         "confidence_score": analysis.confidence
     }
+
+
+if __name__ == "__main__":
+    print("start..")
+    cave_spider_context = """SOURCE: Cave_spider.txt\n A cave spider is a smaller spider variant that behaves similarly, but has less health and inflicts Poison with its attacks. They are only spawned by spawners in various structures, or they can spawn naturally in sulfur caves.[upcoming: Chaos Cubed]
+
+
+    == Spawning ==
+
+    The cave spider is one of the only two mobs in the game to spawn exclusively from spawners,[until: Chaos Cubed] the other being the breeze.
+
+
+    === Monster spawners ===
+    Cave spiders spawn from monster spawners in mineshafts at a light level of 0. These monster spawners are surrounded by cobwebs in corridors.
+
+
+    === Trial spawners ===
+    Cave spiders have a 25% chance to be selected as the "small melee" mob for trial spawners in trial chambers.
+
+
+    === Spider jockeys ===
+
+    There is a 1% chance for a cave spider to spawn with a skeleton riding it, forming a cave spider jockey. The skeleton has an 80% chance to be replaced by a stray, bogged, parched, or wither skeleton in the biomes where they spawn. Similar to regular spiders, the skeleton controls how both mobs move. Cave spider jockeys can fit through smaller gaps than spider jockeys.
+
+
+    == Drops ==
+
+
+    === On death ===
+    Java Edition:Bedrock Edition:
+    5XP when killed by a player or tamed wolf.
+
+
+    == Behavior ==
+    Cave spiders inherit their behavior from spiders:
+
+    They are neutral if under daylight or in light levels of 12 or above, otherwise hostile toward players and iron golems. Once they become hostile, light does not affect them.
+    They attack by leaping and biting.
+    They are unaffected by cobwebs and Poison.
+    They are scared of armadillos.
+    They have some distinctions from regular spiders:
+
+    They can fit through a space that is one block wide and 12 block tall.
+    They can go through the spaces between two different types of (unconnected) fences.
+    They cannot spawn with status effects in Hard difficulty.[Java Edition  only]
+    They flip 90 upon death.[Bedrock Edition  only]
+    Being arthropods, they are weak against weapons with the Bane of Arthropods enchantment.
+    Unlike other neutral mobs, cave spiders don't count towards the AngryAt tag.
+    The red eyes of cave spiders are emissive with Vibrant Visuals, making them easily distinguishable in the dark caves where they spawn.
+
+
+    === Poison ===
+    On Normal or Hard difficulty, cave spiders inflict Poison upon attacking. On Normal difficulty, the Poison lasts for 7 seconds and causes 6HP damage. On Hard difficulty, it lasts for 15 seconds and causes 12HP  6. Poison damage is not taken when the player's health is at 1HP.
+    """
+
+    cave_spider_doc = Document(
+    page_content=cave_spider_context,
+    metadata={
+        "source": "Cave_spider.txt",
+        "page_number": "1",
+    },
+    )
+
+    bad_context = """
+    The sky is blue and the grass is green. I like Minecraft.
+    """
+
+    bad_doc = Document(
+    page_content=bad_context,
+    metadata={
+        "source": "I_made_it_up.txt",
+        "page_number": "1",
+    },
+    )
+
+    # test the agent with correct context
+    state = {
+        "question": "How much damage do cave spiders do? Can they spawn in the trial chambers?",
+        "retrieved_chunks": [cave_spider_doc]
+    }
+    result = analyst_node(state)
+    print("with good context:", result)
+
+    # test the agent with irrelevant context
+    state = {
+        "question": "How much damage do cave spiders do? Can they spawn in the trial chambers?",
+        "retrieved_chunks": [bad_doc]
+    }
+    result = analyst_node(state)
+    print("with bad context:", result)
